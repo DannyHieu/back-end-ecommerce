28e9ea5f489ddfaec8243cf9aca36ce592443114
hieunguyen <hieunguyenxuan2012@gmail.com>
2023-09-03 11:54:04 +0700

    first commit

---
+++ src/main/java/coder/nxhieu/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package coder.nxhieu.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coder/nxhieu/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package coder.nxhieu.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
